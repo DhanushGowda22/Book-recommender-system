05bdb50604198490207587fabac8103fdc2478cb
DhanushGowda22 <appudhanush224@gmail.com>
2026-03-28 13:33:53 +0530

    fixed image error

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ if st.button('Show Recommendation'):
 
     for i in range(5):
         with cols[i]:
-            st.image(poster_url[i], use_container_width=True)
+            st.image(poster_url[i], width=150)
             st.markdown(f"<p style='text-align:center'>{recommended_books[i]}</p>", unsafe_allow_html=True)
 
 st.markdown("---")
